fc41ae159c2a2b580dac450c2c8219bc87c1f62a
Bu-geon <bearbu@naver.com>
2023-08-14 00:12:15 +0900

    [FEAT]change component directory and add class svgs, update navigation list

---
+++ front-end/components/navigation/index.tsx
@@ -0,0 +1,83 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { usePathname } from 'next/navigation'
+
+import styles from './navigation.module.scss'
+
+import homeIcon from '@svg/home.svg'
+import homeActiveIcon from '@svg/homeActive.svg'
+import ticketIcon from '@svg/ticket.svg'
+import ticketActiveIcon from '@svg/ticketActive.svg'
+import communityIcon from '@svg/community.svg'
+import communityActiveIcon from '@svg/communityActive.svg'
+import myPageIcon from '@svg/myPage.svg'
+import myPageActiveIcon from '@svg/myPageActive.svg'
+import classIcon from '@svg/class.svg'
+import classActiveIcon from '@svg/classActive.svg'
+
+export default function Navigation() {
+  const pathname = usePathname()
+
+  return (
+    <nav className={styles.navigation}>
+      <ul>
+        {NAV_LIST.map(({ label, path, icon, activeIcon }) => {
+          const isActive =
+            (path === '/' && pathname === '/') ||
+            (pathname !== '/' && path.startsWith(pathname!))
+
+          return (
+            <li key={path}>
+              <Link
+                className={`${styles.link} ${isActive ? styles.active : ''}`}
+                href={path}
+              >
+                {!isActive && (
+                  <Image className={styles.icon} src={icon} alt={label} />
+                )}
+                {isActive && (
+                  <Image className={styles.icon} src={activeIcon} alt={label} />
+                )}
+                {label}
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </nav>
+  )
+}
+
+// FIXME: 로그인한 User type(고객, 강사, 기업)에 따라 다르게 뿌려야함
+const NAV_LIST = [
+  {
+    label: '홈',
+    path: '/',
+    icon: homeIcon,
+    activeIcon: homeActiveIcon,
+  },
+  {
+    label: '강의',
+    path: '/classes',
+    icon: classIcon,
+    activeIcon: classActiveIcon,
+  },
+  {
+    label: '수강권',
+    path: '/tickets',
+    icon: ticketIcon,
+    activeIcon: ticketActiveIcon,
+  },
+  {
+    label: '커뮤니티',
+    path: '/community',
+    icon: communityIcon,
+    activeIcon: communityActiveIcon,
+  },
+  {
+    label: '마이',
+    path: '/setting',
+    icon: myPageIcon,
+    activeIcon: myPageActiveIcon,
+  },
+]
