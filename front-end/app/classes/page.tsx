0b26b87daf1da5de7fb98618d84713a2850dfde0
Bu-geon <bearbu@naver.com>
2023-08-14 00:24:24 +0900

    [FEAT]add classes page

--- front-end/app/classes/page.tsx
+++ front-end/app/classes/page.tsx
@@ -1,3 +1,38 @@
-export default function Classes() {
-  return <h1>Here's for your classes</h1>
+import CLASSES from './mockupData'
+
+import Calendar from '@components/calendar'
+import ClassCard from './classCard'
+import AddClassButton from './addClassButton'
+
+export default function ClassesPage() {
+  return (
+    <>
+      <Calendar />
+      <ClassCard
+        teacher="김부건"
+        lessonName={'저스티나'}
+        lessonState={'예약가능'}
+        category={'헬스'}
+      />
+      {CLASSES.map((el) => (
+        <ClassCard
+          key={el.classId}
+          teacher={'바다표범'}
+          lessonName={'저스티나'}
+          lessonState={'예약가능'}
+          category={'헬스'}
+        />
+      ))}
+      {/* {classes.map((el, index) => (
+        <ClassCard
+          key={index}
+          teacher={'바다표범'}
+          lessonName={'저스티나'}
+          lessonState={'예약가능'}
+          category={'헬스'}
+        />
+      ))} */}
+      <AddClassButton />
+    </>
+  )
 }
