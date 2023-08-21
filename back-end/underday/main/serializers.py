5e7de82a69b76b6b6810de1a97a203be1292a341
kbule771 <wansuk771@gmail.com>
2023-08-21 21:25:11 +0900

    [feat]class CRUD

--- back-end/underday/main/serializers.py
+++ back-end/underday/main/serializers.py
@@ -1,9 +1,18 @@
 # backend/post/serializers.py
 from rest_framework import serializers
-from .models import UrMaster
+from .models import UrMaster,TrClass,TrMbship
 
 class UrMasterSerializer(serializers.ModelSerializer):
     class Meta:
         model = UrMaster
         fields = '__all__'
 
+class TrClassSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TrClass
+        fields = '__all__'   
+
+class TrMbshipSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TrMbship
+        fields = '__all__'  
--- back-end/underday/underday/settings.py
+++ back-end/underday/underday/settings.py
@@ -38,11 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'main',
-    
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +54,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000'
+#                          ,'http://localhost:3000'
+#                          ]
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'underday.urls'
 
