2aab3e7ffc9010ddf0254d8aa0b125d96933a3ff
Bugeon Kim 김부건 <bearbu@naver.com>
2023-09-25 21:45:43 +0900

    Update settings.py

--- backend/underday/underday/settings.py
+++ backend/underday/underday/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
         'NAME': 'underday',
         'USER': 'root',
         'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'HOST': 'svc.sel5.cloudtype.app',
+        'PORT': '30518',
     }
    
     # 'default': {
