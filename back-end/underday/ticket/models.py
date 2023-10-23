5d11e626d9636c3cb1c2042e4bf07583653a4764
김도완 <kim.raon930319@gmail.com>
2023-10-23 21:52:11 +0900

    [feat] ticket API

--- back-end/underday/ticket/models.py
+++ back-end/underday/ticket/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class UrMaster(models.Model):
+    user_numb = models.CharField(primary_key=True, max_length=8)  # Field name made lowercase.
+    user_idxx = models.CharField(db_column='USER_IDXX', max_length=8, blank=True, null=True)  # Field name made lowercase.
+    user_phon = models.CharField(db_column='USER_PHON', max_length=20, blank=True, null=True)  # Field name made lowercase.
+    user_name = models.CharField(db_column='USER_NAME', max_length=20, blank=True, null=True)  # Field name made lowercase.
+    user_abcd = models.CharField(db_column='USER_ABCD', max_length=4, blank=True, null=True)  # Field name made lowercase.
+    user_pasw = models.CharField(db_column='USER_PASW', max_length=20, blank=True, null=True)  # Field name made lowercase.
+    user_pwer = models.IntegerField(db_column='USER_PWER', max_length=1, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'ur_master'
+
 class UrMbship(models.Model):
     umem_numb = models.CharField(db_column='UMEM_NUMB', primary_key=True, max_length=10)
     umem_stat = models.CharField(db_column='UMEM_STAT', max_length=8, blank=True, null=True)
@@ -16,3 +29,22 @@ class UrMbship(models.Model):
     class Meta:
         managed = False
         db_table = 'ur_mbship'
+
+class TrMbship(models.Model):
+    tmem_numb = models.CharField(db_column='TMEM_NUMB', primary_key=True, max_length=8)
+    user_numb = models.CharField(db_column='USER_NUMB', max_length=8, blank=True, null=True)
+    tmem_name = models.CharField(db_column='TMEM_NAME', max_length=20, blank=True, null=True)
+    tmem_expl= models.CharField(db_column='TMEM_EXPL', max_length=300, blank=True, null=True)
+    tmem_cate = models.CharField(db_column='TMEM_CATE', max_length=5, blank=True, null=True)
+    tmem_grop = models.CharField(db_column='TMEM_GROP', max_length=1, blank=True, null=True)
+    tmem_alr1 = models.CharField(db_column='TMEM_ALR1', max_length=2, blank=True, null=True)
+    tmem_alr2 = models.CharField(db_column='TMEM_ALR2', max_length=2, blank=True, null=True)
+    tmem_maxx = models.CharField(db_column='TMEM_MAXX', max_length=3, blank=True, null=True)
+    tmem_wait = models.CharField(db_column='TMEM_WAIT', max_length=2, blank=True, null=True)
+    tmem_ysno = models.CharField(db_column='TMEM_YSNO', max_length=1)
+    tmem_inst = models.DateTimeField(db_column='TMEM_INST', blank=True, null=True)
+    tmem_updt = models.DateTimeField(db_column='TMEM_UPDT', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table ='tr_mbship'
--- back-end/underday/ticket/views.py
+++ back-end/underday/ticket/views.py
@@ -1,7 +1,7 @@
 import json
 
 from django.shortcuts import render
-from .models import UrMbship
+from .models import UrMaster, UrMbship, TrMbship
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from . import views
@@ -11,29 +11,52 @@ from . import views
 # 티켓리스트
 @api_view(['POST'])
 def ticket_list(request):
+
     try:
         
-        if UrMbship.objects.filter(user_numb=request.data["user_numb"]).exists():
-            
-            ticketInfo = UrMbship.objects.get(user_numb=request.data["user_numb"])
+        # 사용자ID, 권한(회원) chk
+        if UrMaster.objects.filter(user_numb=request.data["user_numb"]).exists():
 
-        # umem_numb : "string”, — 티켓일련번호
-        # umem_stat : "string”, — 회원권이용시작일시
-        # umem_endt : "string”, — 회원권이용종료일시
-        # tmem_numb : "string", - 강사수업일련번호
-        # umem_tnum : "string”, — 회원권등록회차
-        # umem_unum : "string”, — 회원권사용회차
-        # umem_ysno : "string”, — 회원권사용가능여부
-        # umem_inst : "string”, — 회원권등록일시
+            userInfo = UrMaster.objects.get(user_numb=request.data["user_numb"])
 
-            return Response({'umem_numb': ticketInfo.umem_numb
-                            ,'umem_stat': ticketInfo.umem_stat
-                            ,'umem_endt': ticketInfo.umem_endt
-                            ,'tmem_numb': ticketInfo.tmem_numb
-                            ,'umem_tnum': ticketInfo.umem_tnum
-                            ,'umem_unum': ticketInfo.umem_unum
-                            ,'umem_ysno': ticketInfo.umem_ysno
-                            ,'umem_inst': ticketInfo.umem_inst}, status=200)
+            # user_abcd 회원구분(A:회원, B:강사, C:기업)
+            if(userInfo.user_abcd == "A"){
+                
+                ticketInfo = UrMbship.objects.get(user_numb=request.data["user_numb"])
+
+                return Response([{'umem_numb': ticketInfo.umem_numb # 회원권일련번호
+                                ,'user_numb': ticketInfo.user_numb # 사용자일련번호
+                                ,'tmem_numb': ticketInfo.tmem_numb # 강사수업일련번호
+                                ,'umem_stat': ticketInfo.umem_stat # 회원권이용시작일자
+                                ,'umem_endt': ticketInfo.umem_endt # 회원권이용종료일자
+                                ,'umem_tnum': ticketInfo.umem_tnum # 회원권등록회차
+                                ,'umem_unum': ticketInfo.umem_unum # 회원권사용회차
+                                ,'umem_ysno': ticketInfo.umem_ysno # 회원권사용가능여부
+                                }], status=200)
+
+            } else if(userInfo.user_abcd == "B"){
+                
+                # 강사 본인이 갖고있는 강사수업 LIST 조회
+                trMbshipList = TrMbship.objects.get(user_numb=user_numb)
+
+                # TODO 강사가 소유하고 있는 강사수업일련번호 다건일 경우 오류 날듯 개선 필요
+                ticketInfo = UrMbship.objects.get(tmem_numb=trMbshipList.tmem_numb)
+                
+                return Response([{'umem_numb': ticketInfo.umem_numb # 회원권일련번호
+                                ,'user_numb': ticketInfo.user_numb # 사용자일련번호
+                                ,'tmem_numb': ticketInfo.tmem_numb # 강사수업일련번호
+                                ,'umem_stat': ticketInfo.umem_stat # 회원권이용시작일자
+                                ,'umem_endt': ticketInfo.umem_endt # 회원권이용종료일자
+                                ,'umem_tnum': ticketInfo.umem_tnum # 회원권등록회차
+                                ,'umem_unum': ticketInfo.umem_unum # 회원권사용회차
+                                ,'umem_ysno': ticketInfo.umem_ysno # 회원권사용가능여부
+                                }], status=200)
+
+            } else if(userInfo.user_abcd == "C"){
+                # TODO 기업일 경우 리스트 조회 추가 예정
+            } else {
+                return Response({'message': 'INVAILD_USERS'}, status=401)
+            }
 
         else :
             return Response({'message': '일치하는 ID가 없습니다.'}, status=401)
@@ -47,22 +70,47 @@ def ticket_list(request):
 # 티켓생성
 @api_view(['POST'])
 def create(request):
-    try:
-        # 입력 생성 전 필수입력체크
-            # TMEM_NUMB '강사수업일련번호' 
 
-        if UrMbship.objects.filter(user_idxx=request.data['user_idxx']).exists():
+    try:
+        userNumb = "";
+
+        # 강사수업일련번호 여부 chk, 권한 chk
+        if TrMbship.objects.filter(tmem_numb=request.data['tmem_numb']).exists():
+            
+            trMbshipInfo = TrMbship.objects.get(tmem_numb=request.data["tmem_numb"])
+
+            # TODO JWT 토큰 연동시 강사수업일련번호 소유 여부 검증 로직 추가
+#             if(trMbshipInfo.user_numb != request.data["user_numb"]):
+# r               eturn Response({'message': ''}, status=401) 
+
+            userInfo = UrMaster.objects.get(user_numb=trMbshipInfo.user_numb)
+
+            # user_abcd 회원구분(A:회원, B:강사, C:기업)
+            if(userInfo.user_abcd != "B"){
+                return Response({'message': '티켓 생성 권한이 없습니다.'}, status=401)
+            }
+        else :
+            return Response({'message': '일치하는 강사수업일련번호가 없습니다.'}, status=401)
+
+
+        # 회원ID 유효여부 체크
+        if UrMaster.objects.filter(user_idxx=request.data['user_idxx']).exists():
+            userInfo = UrMaster.objects.get(user_idxx=trMbshipInfo.user_idxx)
+
+            userNumb = userInfo.user_numb
+        else :
             return Response({'message': 'INVAILD_USERS'}, status=401)
 
         # 티켓생성
         UrMbship.objects.create(
-            umem_stat=request.data["umem_stat"],
-            umem_endt=request.data["umem_endt"],
-            tmem_numb=request.data["tmem_numb"],
-            umem_tnum=request.data["umem_tnum"],
-            umem_unum=request.data["umem_unum"],
-            umem_ysno=request.data["umem_ysno"],
-            umem_inst=request.data["umem_inst"]
+            umem_numb=request.data["umem_numb"], # 회원권일련번호
+            user_numb=userNumb, # 사용자일련번호
+            tmem_numb=request.data["tmem_numb"], # 강사수업일련번호
+            umem_stat=request.data["umem_stat"], # 회원권이용시작일자
+            umem_endt=request.data["umem_endt"], # 회원권이용종료일자
+            umem_tnum=request.data["umem_tnum"], # 회원권등록회차
+            umem_unum=request.data["umem_unum"], # 회원권사용회차
+            umem_ysno=request.data["umem_ysno"]  # 회원권사용가능여부
         )
     
         return Response({'message': 'SUCCESS'}, status=201)
@@ -70,53 +118,39 @@ def create(request):
     except KeyError:
         return Response({'message': 'KEY_ERROR'}, status=400)
 
-        
-# 티켓 조회, 추가
-@api_view(['GET','POST'])
-def ticket(request):
+
+#  강사수업리스트 조회 API
+@csrf_exempt
+@api_view(['POST'])
+def trMbshipList(request):
 
     try:
-        if(request.method == 'GET'):
-
-            user_numb = request.GET['userId']
-
-            if UrMbship.objects.filter(user_numb=user_numb).exists():
-                
-                ticketInfo = UrMbship.objects.get(user_numb=user_numb)
-
-            # umem_numb : "string”, — 티켓일련번호
-            # umem_stat : "string”, — 회원권이용시작일시
-            # umem_endt : "string”, — 회원권이용종료일시
-            # tmem_numb : "string", - 강사수업일련번호
-            # umem_tnum : "string”, — 회원권등록회차
-            # umem_unum : "string”, — 회원권사용회차
-            # umem_ysno : "string”, — 회원권사용가능여부
-            # umem_inst : "string”, — 회원권등록일시
-
-                return Response({'umem_numb': ticketInfo.umem_numb
-                                ,'umem_stat': ticketInfo.umem_stat
-                                ,'umem_endt': ticketInfo.umem_endt
-                                ,'tmem_numb': ticketInfo.tmem_numb
-                                ,'umem_tnum': ticketInfo.umem_tnum
-                                ,'umem_unum': ticketInfo.umem_unum
-                                ,'umem_ysno': ticketInfo.umem_ysno}, status=200)
-
-            else :
-                return Response({'message': '일치하는 ID가 없습니다.'}, status=401)
-
-        elif(request.method == 'POST'):
+        # 강사토큰의 사용자일련번호 체크, 권한 chk
+        if UrMaster.objects.filter(user_numb=request.data['user_numb']).exists():
             
-            # 티켓생성
-            UrMbship.objects.create(
-                umem_stat=request.data["umem_stat"],
-                umem_endt=request.data["umem_endt"],
-                tmem_numb=request.data["tmem_numb"],
-                umem_tnum=request.data["umem_tnum"],
-                umem_unum=request.data["umem_unum"],
-                umem_ysno=request.data["umem_ysno"]
-            )
+            userInfo = UrMaster.objects.get(user_numb=request.data["user_numb"])
             
-            return Response({'message': 'SUCCESS'}, status=200)
+            # user_abcd 회원구분(A:회원, B:강사, C:기업)
+            if(userInfo.user_abcd != "B"){
+                return Response({'message': '조회권한이 없습니다.'}, status=401)
+            }
+        else :
+            return Response({'message': 'INVAILD_USERS'}, status=401)
+            
+        # 강사 본인이 갖고있는 강사수업 LIST 조회
+        if TrMbship.objects.filter(user_numb=request.data['user_numb']).exists():
+
+            trMbshipList = TrMbship.objects.get(user_numb=user_numb)
+
+            return Response([{'tmem_numb': trMbshipList.umem_numb # 강사수업일련번호
+                            ,'tmem_name': trMbshipList.tmem_name # 강의명
+                            ,'tmem_expl': trMbshipList.tmem_expl # 강의실명
+                            }], status=200)
+        else : 
+            return Response({'tmem_numb': '' # 강사수업일련번호
+                            ,'tmem_name': '' # 강의명
+                            ,'tmem_expl': '' # 강의실명
+                            }, status=201)
 
     except KeyError:
         return Response({'message': 'KEY_ERROR'}, status=400)
