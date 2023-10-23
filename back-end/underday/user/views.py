078426909da64fdd50e130401f98e6724b3504a2
김도완 <kim.raon930319@gmail.com>
2023-10-23 21:54:32 +0900

    [feat] login, signup API

--- back-end/underday/user/views.py
+++ back-end/underday/user/views.py
@@ -23,25 +23,55 @@ def signup(request):
         if request.data["password1"] != request.data["password2"]:
             return Response({'message': 'INVAILD_USERS'}, status=401)
 
-        
-        ## 필수 입력값 모두 체크 후 회원가입 진행
 
         # 비밀번호 암호화
         hashed_password = bcrypt.hashpw(
             request.data["password1"].encode('utf-8'), bcrypt.gensalt())
         decoded_password = hashed_password.decode('utf-8')
         
-        # 회원가입
-        UrMaster.objects.create(
-            user_idxx=request.data["user_idxx"],
-            user_pasw=decoded_password,
-            user_phon=request.data["user_phon"],
-            user_name=request.data["user_name"],
-            user_abcd=request.data["user_abcd"], #사용자구분 (회원,강사, 기업(A,B,C 알파벳 한자리)
-            user_pwer=0
-            # user_orig=request.data["user_orig"], #소속명
-            # user_addr=request.data["user_addr"], #도로명주소
-        )
+        ## user_abcd 회원구분(A:회원, B:강사, C:기업)
+        if(request.data["user_abcd"] == "A"){
+
+            UrMaster.objects.create(
+                user_idxx=request.data["user_idxx"],
+                user_pasw=decoded_password,
+                user_phon=request.data["user_phon"],
+                user_name=request.data["user_name"],
+                user_abcd=request.data["user_abcd"],
+                user_pwer=0,
+                user_rptt="Y",
+                user_sumo="SUN"
+            )
+        } else if(request.data["user_abcd"] == "B"){
+
+            UrMaster.objects.create(
+                user_idxx=request.data["user_idxx"],
+                user_pasw=decoded_password,
+                user_phon=request.data["user_phon"],
+                user_name=request.data["user_name"],
+                user_abcd=request.data["user_abcd"], #사용자구분 (회원,강사, 기업(A,B,C 알파벳 한자리)
+                user_pwer=0,
+                user_rptt="Y",
+                user_sumo="SUN",
+                user_orig=request.data["user_orig"], #소속명
+            )
+        } else if(request.data["user_abcd"] == "C"){
+
+            UrMaster.objects.create(
+                user_idxx=request.data["user_idxx"],
+                user_pasw=decoded_password,
+                user_phon=request.data["user_phon"],
+                user_name=request.data["user_name"],
+                user_abcd=request.data["user_abcd"], #사용자구분 (회원,강사, 기업(A,B,C 알파벳 한자리)
+                user_pwer=0,
+                user_rptt="Y",
+                user_sumo="SUN",
+                user_orig=request.data["user_orig"], #소속명
+                user_addr=request.data["user_addr"], #도로명주소
+            )
+        } else {
+            return Response({'message': 'INVAILD_USERS'}, status=401)
+        }
     
         return Response({'message': 'SUCCESS'}, status=201)
 
@@ -60,7 +90,7 @@ def login(request):
         # 아이디 검증 - PYTHON에서 선처리하는 공간 or JWT 검증하는 공간에서 아이디 검증하므로 제외될 예정.
         if UrMaster.objects.filter(user_idxx=request.data["user_idxx"]).exists():
             
-            user = UrMaster.objects.get(user_idxx=request.data["user_idxx"])
+            userInfo = UrMaster.objects.get(user_idxx=request.data["user_idxx"])
 
             # 비밀번호 오류 count 체크
             # 5회 이상이면 휴대폰 인증 추가!
@@ -69,11 +99,11 @@ def login(request):
             
             # PW 검증
             # db에 저장된 암호화 PW == 입력받고 암호화된 PW 비교
-            if bcrypt.checkpw(request.data['password'].encode('utf-8'), user.user_pasw.encode('utf-8')) == True:
+            if bcrypt.checkpw(request.data['password'].encode('utf-8'), userInfo.user_pasw.encode('utf-8')) == True:
                 return Response({'message': 'SUCCESS'}, status=200)
             else :
                 # 비밀번호 오류 count 추가
-                userPwer = user.user_pwer + 1
+                userPwer = userInfo.user_pwer + 1
 
                 UrMaster.objects.filter(user_idxx=request.data["user_idxx"]).update(user_pwer=userPwer)
                 
@@ -86,16 +116,29 @@ def login(request):
         return Response({'message': 'KEY_ERROR'}, status=400)
 
 
+# 아이디 중복 체크 API
+@csrf_exempt
+@api_view(['POST'])
+def chkUserId(request):
+    try:
+        if UrMaster.objects.filter(user_idxx=request.data["user_idxx"]).exists():
+            return Response({'message': '중복된 ID입니다.'}, status=401)
+        else :
+            return Response({'message': 'SUCCESS'}, status=200)
+
+    except KeyError:
+        return Response({'message': 'KEY_ERROR'}, status=400)
+
+
 # 마이페이지 조회, 추가, 수정, 삭제
 @api_view(['GET','POST','PATCH','DELETE'])
 def mypage(request):
-
     try:
         if(request.method == 'GET'):
 
             user_idxx = request.get[userId]
             
-            user = UrMaster.objects.get(user_idxx=user_idxx)
+            userInfo = UrMaster.objects.get(user_idxx=user_idxx)
 
             # UrMaster.objects.update(
                 
