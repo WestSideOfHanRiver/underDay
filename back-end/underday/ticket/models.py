96283ed861382ce9f5d75d12daef160ecbb0b9ba
김도완 <kim.raon930319@gmail.com>
2023-10-24 21:05:32 +0900

    [fix] models update

--- back-end/underday/ticket/models.py
+++ back-end/underday/ticket/models.py
@@ -8,11 +8,11 @@ class UrMaster(models.Model):
     user_name = models.CharField(db_column='USER_NAME', max_length=20, blank=True, null=True)  # Field name made lowercase.
     user_abcd = models.CharField(db_column='USER_ABCD', max_length=4, blank=True, null=True)  # Field name made lowercase.
     user_pasw = models.CharField(db_column='USER_PASW', max_length=20, blank=True, null=True)  # Field name made lowercase.
-    user_pwer = models.IntegerField(db_column='USER_PWER', max_length=1, blank=True, null=True)  # Field name made lowercase.
-    user_rptt = models.IntegerField(db_column='USER_RPTT', max_length=2, blank=True, null=True)  # Field name made lowercase.
-    user_sumo = models.IntegerField(db_column='USER_SUMO', max_length=4, blank=True, null=True)  # Field name made lowercase.
-    user_orig = models.IntegerField(db_column='USER_ORIG', max_length=100, blank=True, null=True)  # Field name made lowercase.
-    user_addr = models.IntegerField(db_column='USER_ADDR', max_length=200, blank=True, null=True)  # Field name made lowercase.
+    user_pwer = models.CharField(db_column='USER_PWER', max_length=1, blank=True, null=True)  # Field name made lowercase.
+    user_rptt = models.CharField(db_column='USER_RPTT', max_length=2, blank=True, null=True)  # Field name made lowercase.
+    user_sumo = models.CharField(db_column='USER_SUMO', max_length=4, blank=True, null=True)  # Field name made lowercase.
+    user_orig = models.CharField(db_column='USER_ORIG', max_length=100, blank=True, null=True)  # Field name made lowercase.
+    user_addr = models.CharField(db_column='USER_ADDR', max_length=200, blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
         managed = False
