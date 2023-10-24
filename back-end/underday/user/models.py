e7dbd1eef00c728f2c3f225e541bf748a63f4894
김도완 <kim.raon930319@gmail.com>
2023-10-24 20:59:33 +0900

    [fix] ur_master models ADD

--- back-end/underday/user/models.py
+++ back-end/underday/user/models.py
@@ -9,6 +9,10 @@ class UrMaster(models.Model):
     user_abcd = models.CharField(db_column='USER_ABCD', max_length=4, blank=True, null=True)  # Field name made lowercase.
     user_pasw = models.CharField(db_column='USER_PASW', max_length=20, blank=True, null=True)  # Field name made lowercase.
     user_pwer = models.IntegerField(db_column='USER_PWER', max_length=1, blank=True, null=True)  # Field name made lowercase.
+    user_rptt = models.IntegerField(db_column='USER_RPTT', max_length=2, blank=True, null=True)  # Field name made lowercase.
+    user_sumo = models.IntegerField(db_column='USER_SUMO', max_length=4, blank=True, null=True)  # Field name made lowercase.
+    user_orig = models.IntegerField(db_column='USER_ORIG', max_length=100, blank=True, null=True)  # Field name made lowercase.
+    user_addr = models.IntegerField(db_column='USER_ADDR', max_length=200, blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
         managed = False
