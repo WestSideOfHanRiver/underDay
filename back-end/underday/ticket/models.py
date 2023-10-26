6328acc086ddc0916d4fa9aff44bb89010197ba4
김도완 <kim.raon930319@gmail.com>
2023-10-26 21:03:01 +0900

    [fix] models update

--- back-end/underday/ticket/models.py
+++ back-end/underday/ticket/models.py
@@ -38,7 +38,7 @@ class TrMbship(models.Model):
     tmem_numb = models.CharField(db_column='TMEM_NUMB', primary_key=True, max_length=8)
     user_numb = models.CharField(db_column='USER_NUMB', max_length=8, blank=True, null=True)
     tmem_name = models.CharField(db_column='TMEM_NAME', max_length=20, blank=True, null=True)
-    tmem_expl= models.CharField(db_column='TMEM_EXPL', max_length=300, blank=True, null=True)
+    tmem_expl = models.CharField(db_column='TMEM_EXPL', max_length=300, blank=True, null=True)
     tmem_cate = models.CharField(db_column='TMEM_CATE', max_length=5, blank=True, null=True)
     tmem_grop = models.CharField(db_column='TMEM_GROP', max_length=1, blank=True, null=True)
     tmem_alr1 = models.CharField(db_column='TMEM_ALR1', max_length=2, blank=True, null=True)
