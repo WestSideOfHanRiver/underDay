d2e7e7de71e69c5d8fdbbd10e3245a65fe149c97
김도완 <kim.raon930319@gmail.com>
2023-11-10 23:47:28 +0900

    [fix] ticket urls update

--- back-end/underday/ticket/urls.py
+++ back-end/underday/ticket/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('ticket_list/', views.ticket_list, name='ticket_list'),
     path('create/', views.create, name='create'),
+    path('update/', views.update, name='update'),
     path('trMbshipList/', views.trMbshipList, name='trMbshipList'),
 ]
