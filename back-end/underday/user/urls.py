c19e2a86cdcf896413d21522bdb9bae4bbc3aaaa
김도완 <kim.raon930319@gmail.com>
2023-12-13 20:35:44 +0900

    [fix] token delete

--- back-end/underday/user/urls.py
+++ back-end/underday/user/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from user.jwt_claim_serializer import MyTokenObtainPairView
+# from user.jwt_claim_serializer import MyTokenObtainPairView
 
 # from rest_framework_simplejwt.views import (
 #     TokenObtainPairView,
