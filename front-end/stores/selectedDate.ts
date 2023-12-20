a271ee679b4760ea0dd95bd150749f8d40a6045b
Bu-geon <bearbu@naver.com>
2023-12-20 20:23:50 +0900

    [FEATURE]change state name from RootSstate to SelectedDateState

--- front-end/stores/selectedDate.ts
+++ front-end/stores/selectedDate.ts
@@ -8,6 +8,6 @@ export const store = configureStore({
   },
 })
 
-export type RootState = ReturnType<typeof store.getState>
+export type SelectedDateState = ReturnType<typeof store.getState>
 
 export type AppDispatch = typeof store.dispatch
